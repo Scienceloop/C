92c53c745c2a320d965599d4e9cb5fc76eadc945
Shounak Das <112923983+Scienceloop@users.noreply.github.com>
2023-09-16 21:43:35 +0530

    Update sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 float sq(float num){
-  float e = 0.001;
+  float e = 0.000001;
   float guess = 0;
   
-  while (guess*guess > num){
+  while (guess*guess < num){
     guess += e;
   }
   return guess;
