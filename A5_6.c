2ebcfd28ec222faddb626a4db6e006368f96c9f4
Shounak Das <112923983+Scienceloop@users.noreply.github.com>
2023-12-05 03:41:48 -0500

    Update A5_6.c

--- A5_6.c
+++ A5_6.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+int reverse(int n){
+        int rev=0,d=0;
+
+        while(n!=0){
+
+                d = n%10;
+                rev = rev*10+d;
+                n /= 10;
+
+        }
+        return rev;
+}
+int main(){
+        int num1=0;
+        int num2=0;
+
+        printf("Enter a number\n");
+        scanf("%d.%d",&num1, &num2);
+        printf("The reversed number is %d.%d\n",reverse(num2), reverse(num1));
+
+        return 0;
+}
