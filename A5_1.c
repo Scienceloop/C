d577c2ed5699237cb76f06a155d9f14550880f68
Shounak Das <112923983+Scienceloop@users.noreply.github.com>
2023-12-05 03:37:04 -0500

    Create A5_1.c

---
+++ A5_1.c
@@ -0,0 +1,24 @@
+
+        //--------------------------------------------------------
+        swap(x, n);
+
+        gcd(x, n);
+
+        //--------------------------------------------------------
+
+        int arr[n];
+
+        printf("Enter the elements of array of element %d\n ", n);
+        for (int i =0; i < n; i++){
+                scanf("%d", &x);
+                arr1[i] = x;
+        }
+
+        sum_arr(n ,arr1);
+
+        //--------------------------------------------------------
+        printf("Enter the string\n");
+        scanf(" %[^\n]s", str);
+        rm_s(str);
+
+-- INSERT --                                                  84,2-9        98%
